03dfa8ed940f971476c860c6b8143f613dda98cf
JohnsonNYC <jkow95@gmail.com>
2020-05-05 12:47:02 -0400

    added color to match user input and quote validation

--- src/script.js
+++ src/script.js
@@ -68,87 +68,87 @@ function init() {
 
 // Start match
 function startMatch() {
-  if (matchWords()) {
-    isPlaying = true;
-    time = currentLevel + 1;
-    showWord(words);
-    wordInput.value = '';
-    score++;
-  }
+    if (matchWords()) {
+        isPlaying = true;
+        time = currentLevel + 1;
+        showWord(words);
+        wordInput.value = '';
+        score++;
+    }
 
-  // If score is -1, display 0
-  if (score === -1) {
-    scoreDisplay.innerHTML = 0;
-  } else {
-    scoreDisplay.innerHTML = score;
-  }
+    // If score is -1, display 0
+    if (score === -1) {
+        scoreDisplay.innerHTML = 0;
+    } else {
+        scoreDisplay.innerHTML = score;
+    }
 }
 
 // Match currentWord to wordInput
 function matchWords() {
-  if (wordInput.value === currentWord.innerHTML) {
-    message.innerHTML = 'Correct!!!';
-    return true;
-  } else {
-    message.innerHTML = '';
-    return false;
-  }
+    if (wordInput.value === currentWord.innerHTML) {
+        message.innerHTML = 'Correct!!!';
+        return true;
+    } else {
+        message.innerHTML = '';
+        return false;
+    }
 }
 
 // Pick & show random word
 function showWord(words) {
-  // Generate random array index
-  const randIndex = Math.floor(Math.random() * words.length);
-  // Output random word
-  currentWord.innerHTML = words[randIndex];
+    // Generate random array index
+    const randIndex = Math.floor(Math.random() * words.length);
+    // Output random word
+    currentWord.innerHTML = words[randIndex];
 }
 
 const words = ["seven", "eight"]
 
 // Countdown timer
 function countdown() {
-  // Make sure time is not run out
-  if (time > 0) {
-    // Decrement
-    time--;
-  } else if (time === 0) {
-    // Game is over
-    isPlaying = false;
-  }
-  // Show time
-  timeDisplay.innerHTML = time;
+    // Make sure time is not run out
+    if (time > 0) {
+        // Decrement
+        time--;
+    } else if (time === 0) {
+        // Game is over
+        isPlaying = false;
+    }
+    // Show time
+    timeDisplay.innerHTML = time;
 }
 
 // Check game status
 function checkStatus() {
-  if (!isPlaying && time === 0) {
-    message.innerHTML = 'Game Over!!!';
-    score = -1;
-  }
+    if (!isPlaying && time === 0) {
+        message.innerHTML = 'Game Over!!!';
+        score = -1;
+    }
 }
 
 function listenForUsername() {
-  const form = document.querySelector(".form")
-  form.addEventListener("submit", (e) => { 
-    event.preventDefault(); 
-    fetch("https://127.0.0.1:3000/users", {
-      method: "POST", 
-      headers, 
-      body: JSON.stringify({username: e.target.input.value})
+    const form = document.querySelector(".form")
+    form.addEventListener("submit", (e) => {
+        event.preventDefault();
+        fetch("https://127.0.0.1:3000/users", {
+            method: "POST",
+            headers,
+            body: JSON.stringify({ username: e.target.input.value })
+        })
+            .then(resp => resp)
+            .then(data => console.log(data)) //Save data value to username global  
     })
-    .then(resp => resp)
-    .then(data => console.log(data)) //Save data value to username global  
-  })
 }
 
 function revealLogInForm() {
-  const form = document.querySelector(".form")
-  if (form.style.display === "none") { 
-    form.style.display = "block"    
-  } else if (form.style.display === "block") { 
-    form.style.display = "none"
-  }
+    const form = document.querySelector(".form")
+    if (form.style.display === "none") {
+        form.style.display = "block"
+    } else if (form.style.display === "block") {
+        form.style.display = "none"
+    }
 }
 
-    
+
 
